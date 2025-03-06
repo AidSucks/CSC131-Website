3390e9e0f5c15ae617da508df6681598fee69e57
Aidan Anderson <aidananderson167@gmail.com>
2025-03-06 14:39:46 -0800

    Fix: Separate Page Components Added PageComponents.tsx, routes "page.tsx" files cannot have dual exports

--- src/app/(home)/contact/page.tsx
+++ src/app/(home)/contact/page.tsx
@@ -1,15 +1,6 @@
 import PageTitle from "@/app/components/PageTitle";
+import {ContactPageComponent} from "@/app/components/PageComponents";
 
-export function ContactPageComponent() {
-  return(
-    <div>
-      <h1>Contact Us</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, cupiditate. Accusamus inventore magnam nihil, consequatur saepe est doloremque quas nostrum fuga esse temporibus similique recusandae tenetur. Quas enim temporibus aperiam?
-      </p>  
-    </div>
-  );
-}
 
 export default function Page() {  
   return(
