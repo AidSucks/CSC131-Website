665c249ea6f3215ff0674a206c9c84f8179df487
AhmedKhalaf-97 <ahmedkhalafx97@gmail.com>
2025-03-08 11:42:05 -0800

    Made some changes to how the pages fetch the business info

--- src/app/components/Topbar.tsx
+++ src/app/components/Topbar.tsx
@@ -1,16 +1,14 @@
-import { fetchClientInfo } from "./ClientInfo";
+import businessInfo from "../../../public/data/businessInfo.json";
 
-export default async function Topbar() {
-  const  clientInfo  = await fetchClientInfo();
-
+export default function Topbar() {
   return (
     <div className="container-fluid bg-dark px-5 d-none d-lg-block">
     <div className="row gx-0">
       <div className="col-lg-8 text-center text-lg-start mb-2 mb-lg-0">
         <div className="d-inline-flex align-items-center" style={{height: 45}}>
-          <small className="me-3 text-light"><i className="fa fa-map-marker-alt me-2"></i>{clientInfo.address}</small>
-          <small className="me-3 text-light"><i className="fa fa-phone-alt me-2"></i>{clientInfo.phone}</small>
-          <small className="text-light"><i className="fa fa-envelope-open me-2"></i>{clientInfo.email}</small>
+          <small className="me-3 text-light"><i className="fa fa-map-marker-alt me-2"></i>{businessInfo.address}</small>
+          <small className="me-3 text-light"><i className="fa fa-phone-alt me-2"></i>{businessInfo.phone}</small>
+          <small className="text-light"><i className="fa fa-envelope-open me-2"></i>{businessInfo.email}</small>
         </div>
       </div>
       <div className="col-lg-4 text-center text-lg-end">
